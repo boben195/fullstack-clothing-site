627bcbdda12176d27c84533064a7ff25cc5caeb3
boben195 <boben195@ukr.net>
2025-03-17 21:26:31 +0200

    style for navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { BiCart, BiUser } from "react-icons/bi";
+import "./Navbar.css";
 
-const navigate = useNavigate();
+// const navigate = useNavigate();
 
 const handleNavigation = (path) => {
   navigate(path);
