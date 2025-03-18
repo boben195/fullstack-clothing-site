5812c83aa8fd7b3f8a2352510e22eab07839dbd5
boben195 <boben195@ukr.net>
2025-03-18 17:51:30 +0200

    add our collection

--- frontend/src/components/HomeCollection/HomeCollection.jsx
+++ frontend/src/components/HomeCollection/HomeCollection.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { ShopContext } from "../../context/ShopContext";
+import { Link } from "react-router-dom";
 
 const HomeCollection = () => {
-  return <div>HomeCollection</div>;
+  const { products } = useContext(ShopContext);
+  const [homeProduct, setHomeProducts] = useState([]);
+
+  useEffect(() => {
+    setHomeProducts(products.slice(0, 10));
+  }, [products]);
+  return (
+    <div>
+      <div className="product-container">
+        <div className="list-header">
+          <h1>Our Collection</h1>
+          <hr className="devider" />
+        </div>
+        <div className="product-grid">
+          {homeProduct.length > 0 ? (
+            homeProduct.map((product) => (
+              <div className="product-cart" key={product._id}>
+                <div className="product-image">
+                  <Link to={`/product/${product._id}`}>
+                    <img src={product.image[0]} alt="your clothes" />
+                  </Link>
+                </div>
+                <h3>{product.name}</h3>
+                <p>${product.price}</p>
+              </div>
+            ))
+          ) : (
+            <p>No product is found</p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomeCollection;
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../../components/Hero/Hero";
 import BestSeller from "../../components/LastestCollecion/BestSeller";
+import HomeCollection from "../../components/HomeCollection/HomeCollection";
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <BestSeller />
+      <HomeCollection />
     </div>
   );
 };
