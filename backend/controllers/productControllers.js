b6ed5726d15e05ab12a54702a0c45242c1b5bc28
boben195 <boben195@ukr.net>
2025-03-21 23:09:05 +0200

    prod contriller

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -46,10 +46,28 @@ const addProduct = async (req, res) => {
 }
 
 const listProduct = async (req, res) => {
+    try {
+        const products = await productModel.find({})
+        res.json({success: true, products})
+        
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: error.message})
+        
+    }
 
 }
 
 const removeProduct = async (req, res) => {
+    try {
+        await productModel.findByIdAndDelete(req.body._id);
+        res.json({success: true, message: "Product deleted"})
+        
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: error.message})
+        
+    }
 
 }
 
