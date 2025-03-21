8cabd46a558d1909fc8a14a07fafbea11f11ed94
boben195 <boben195@ukr.net>
2025-03-21 15:54:13 +0200

    add models and controllers

---
+++ backend/controllers/userController.js
@@ -0,0 +1,28 @@
+const loginUser = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+
+const registerUser = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+
+const adminLogin = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+
+export {loginUser, registerUser, adminLogin}
