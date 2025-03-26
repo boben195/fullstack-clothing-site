68bdae5763ee0b5548564e7178cf68da77236880
boben195 <boben195@ukr.net>
2025-03-26 13:00:16 +0200

    test ignore

--- admin/src/components/Login/Login.jsx
+++ admin/src/components/Login/Login.jsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmitHandler = async (e) => {
+    try {
+      e.preventDefault();
+      console.log(email, password);
+    } catch (error) {}
+  };
   return (
     <div>
       <div className="admin-container">
         <div className="admin-box">
           <h1 className="login-title">Admin Panel</h1>
-          <form>
+          <form onSubmit={onSubmitHandler}>
             <div className="form-group">
               <p className="form-label">Email Adreess</p>
               <input
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
                 type="email"
                 className="form-input"
                 placeholder="Email adreess"
@@ -20,6 +31,8 @@ const Login = () => {
             <div className="form-group">
               <p className="form-label">Password</p>
               <input
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
                 type="password"
                 className="form-input"
                 placeholder="Enter password"
