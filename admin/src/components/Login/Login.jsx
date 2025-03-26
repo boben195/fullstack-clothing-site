1592532eeaff40e395caf9c33a5e5b19011b93a4
boben195 <boben195@ukr.net>
2025-03-26 12:44:30 +0200

    add style for login

--- admin/src/components/Login/Login.jsx
+++ admin/src/components/Login/Login.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import "./Login.css";
 
 const Login = () => {
   return (
     <div>
       <div className="admin-container">
         <div className="admin-box">
-          <h1 className="logit-title">Admin Panel</h1>
+          <h1 className="login-title">Admin Panel</h1>
           <form>
             <div className="form-group">
               <p className="form-label">Email Adreess</p>
