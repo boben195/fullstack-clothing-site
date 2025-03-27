4368c16f05853d5346e90fbff15e008916db900a
boben195 <boben195@ukr.net>
2025-03-27 22:19:39 +0200

    start section list

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { backendUrl } from "../../App";
 
 const List = () => {
-  return <div>List</div>;
+  const [list, setList] = useState([]);
+  const fetchList = async () => {
+    try {
+      const response = await axios.get(backendUrl);
+    } catch (error) {}
+  };
+
+  return (
+    <div>
+      <p className="product-title">Product List</p>
+      <div className="product-list-container">
+        <div className="product-table-title">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b className="action-title">Action</b>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default List;
